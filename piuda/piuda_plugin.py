f0af7dccb993a5ce92db4d64a91c5a9dd119b3b7
Fenhl <fenhl@fenhl.net>
2016-07-07 00:05:23 +0000

    Say hello

--- piuda/piuda_plugin.py
+++ piuda/piuda_plugin.py
@@ -2,5 +2,24 @@ from spockbot.plugins.base import PluginBase, pl_announce
 
 @pl_announce('PiudaPlugin')
 class PiudaPlugin(PluginBase):
-    requires = ()
-    events = {}
+    requires = ('Chat', 'ClientInfo', 'Movement')
+    events = {
+        'client_join_game': 'perform_initial_actions',
+        'client_position_update': 'start_bot'
+    }
+
+    def __init__(self, ploader, settings):
+        super(PiudaPlugin, self).__init__(ploader, settings)
+
+        self.started = False
+
+    def perform_initial_actions(self, name, data):
+        # Say hello
+        self.chat.chat('Eana!')
+
+    def start_bot(self, _, __):
+        if self.started:
+            return
+        else:
+            self.started = True
+        pass #TODO bot actions
